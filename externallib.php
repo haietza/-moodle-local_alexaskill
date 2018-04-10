5e6416992ae3ce56321832c5888b8929014b2477
Michelle Melton <meltonml@appstate.edu>
2018-04-10 19:45:31 -0400

    Implement GetGradesIntent

--- externallib.php
+++ externallib.php
@@ -27,9 +27,14 @@ require_once($CFG->libdir . "/externallib.php");
 require_once($CFG->dirroot . '/mod/forum/externallib.php');
 
 class local_alexaskill_external extends external_api {
+    
+    /**
+     * Returns description of method parameters
+     * @return external_function_parameters
+     */
     public static function alexa_parameters() {
         return new external_function_parameters(array(
-                'request' => new external_value(PARAM_TEXT)
+                'request' => new external_value(PARAM_TEXT, 'JSON request as a string')
         ));
     }
     
@@ -41,6 +46,9 @@ class local_alexaskill_external extends external_api {
             switch($json["request"]["intent"]["name"]) {
                 case "GetSiteAnnouncementsIntent":
                     $text = self::get_site_announcements(1);
+                    break;
+                case "GetGradesIntent":
+                    $text = self::get_grades();
                     break;
             }
         } else {
@@ -65,49 +73,26 @@ class local_alexaskill_external extends external_api {
         return 'Welcome to As You Learn';
     }
     
+    /**
+     * Returns description of method return values
+     * @return external_single_structure
+     */
     public static function alexa_returns() {
         return new external_single_structure(array(
-                'version' => new external_value(PARAM_TEXT),
+                'version' => new external_value(PARAM_TEXT, 'version number'),
                 'response' => new external_single_structure(array(
                         'outputSpeech' => new external_single_structure(array(
-                                'type' => new external_value(PARAM_TEXT),
-                                'text' => new external_value(PARAM_TEXT)
+                                'type' => new external_value(PARAM_TEXT, 'type of speech output'),
+                                'text' => new external_value(PARAM_TEXT, 'text string to speak')
                         )),
-                        'shouldEndSession' => new external_value(PARAM_BOOL)
+                        'shouldEndSession' => new external_value(PARAM_BOOL,'true if responses ends session')
                 ))
         ));
     }
     
     /**
-     * The function itself
-     * @return string welcome message
-     */
-    public static function hello_world($welcomemessage = 'Hello ') {
-        global $USER;
-        
-        // Parameters validation
-        $params = self::validate_parameters(self::hello_world_parameters(),
-                array('welcomemessage' => $welcomemessage));
-        
-        // Note: don't forget to validate the context and check capabilities
-        
-        // Context validation
-        // OPTIONAL but in most web service it should present
-        $context = context_user::instance($USER->id);
-        self::validate_context($context);
-        
-        // Capability checking
-        // OPTIONAL but in most web service it should present
-        //if (!has_capability('moodle/user:viewdetails', $context)) {
-        //    throw new moodle_exception('cannotviewprofile');
-        //}
-        
-        return $params['welcomemessage'] . $USER->firstname;
-    }
-    
-    /**
-     * The function itself
-     * @return string welcome message
+     * Function to get front page site announcements
+     * @return string site announcements
      */
     private static function get_site_announcements($id = 1) {        
         global $DB;
@@ -144,5 +129,50 @@ class local_alexaskill_external extends external_api {
         }
         
         return $siteannouncements;
+    }
+    
+    /**
+     * Function to get a user's grades
+     * @return string grades of courses user is currently taking
+     */
+    private static function get_grades() {
+        global $DB, $USER;
+        
+        // Parameters validation
+        //$params = self::validate_parameters(self::get_site_news_parameters(), array('id' => $id));
+        
+        // Note: don't forget to validate the context and check capabilities
+        
+        // Context validation
+        // OPTIONAL but in most web service it should present
+        $context = context_user::instance($USER->id);
+        self::validate_context($context);
+        
+        // Capability checking
+        // OPTIONAL but in most web service it should present
+        if (!has_capability('moodle/grade:view', $context)) {
+            throw new moodle_exception('nopermissiontoviewgrades');
+        }
+        
+        // Make sure SQL query is best way to get grades - no internal function?
+        $sql = 'select mdl_course.fullname, mdl_grade_grades.finalgrade
+                from mdl_grade_grades
+                inner join mdl_grade_items
+                on mdl_grade_grades.itemid = mdl_grade_items.id
+                inner join mdl_course
+                on mdl_grade_items.courseid = mdl_course.id
+                where mdl_grade_grades.userid = :userid
+                AND mdl_grade_items.itemtype = "course"
+                AND mdl_course.visible = 1
+                AND mdl_course.showgrades = 1';
+        $params = array('userid' => $USER->id);
+        
+        $gradesrecords = $DB->get_records_sql($sql, $params);
+        $grades = '';
+        foreach ($gradesrecords as $grade) {
+            $grades .= $grade->fullname . '. ' . $grade->finalgrade . '. ';
+        }
+        
+        return $grades;
     }
 }
