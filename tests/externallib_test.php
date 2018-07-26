47a0628384c6dd3ebb726a121afa41a2ea99b4dd
Michelle Melton <meltonml@appstate.edu>
2018-07-26 16:55:23 -0400

    Add test for get grades, responding to else with yes

--- tests/externallib_test.php
+++ tests/externallib_test.php
@@ -1206,4 +1206,30 @@ class local_alexaskill_externallib_testcase extends externallib_advanced_testcas
         
         $this->assertTrue($expecteda == $actual || $expectedb == $actual);
     }
+    
+    /**
+     * Test get_grades, responding to would you like anything else with yes.
+     */
+    public function test_get_grades_valid_else_yes() {
+        $this->resetAfterTest();
+        $getgrades = self::getMethod('get_grades');
+        
+        local_alexaskill_external::$json['request']['dialogState'] = 'IN_PROGRESS';
+        local_alexaskill_external::$json['request']['intent']['slots']['else']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name'] = 'yes';
+        
+        $actual = $getgrades->invokeArgs(null, array('token' => 'valid'));
+        
+        $this->response['response']['shouldEndSession'] = false;
+        $this->response['response']['outputSpeech']['type'] = 'SSML';
+        
+        $expecteda = $this->response;
+        $expecteda['response']['outputSpeech']['ssml'] = '<speak>You can get site announcements <break time = "350ms"/>'
+                . 'course announcements <break time = "350ms"/>grades <break time = "350ms"/>or due dates. Which would you like?</speak>';
+                
+        $expectedb = $this->response;
+        $expectedb['response']['outputSpeech']['ssml'] = '<speak>I can get you site announcements <break time = "350ms"/>'
+                . 'course announcements <break time = "350ms"/>grades <break time = "350ms"/>or due dates. Which would you like?</speak>';
+                
+        $this->assertTrue($expecteda == $actual || $expectedb == $actual);
+    }
 }
