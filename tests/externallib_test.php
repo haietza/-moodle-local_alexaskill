5ff0dce7dfb78dd91f912447661bccf061cbf793
Michelle Melton <meltonml@appstate.edu>
2018-07-27 18:54:15 -0400

    Add test for get due dates invalid no capability

--- tests/externallib_test.php
+++ tests/externallib_test.php
@@ -1902,4 +1902,57 @@ class local_alexaskill_externallib_testcase extends externallib_advanced_testcas
 
         $this->assertTrue($expecteda == $actual || $expectedb == $actual);
     }
+    
+    /**
+     * Test get_due_dates, invalid no capability.
+     */
+    public function test_get_due_dates_invalid_no_capability() {
+        global $DB;
+        $this->resetAfterTest();
+        $getduedates = self::getMethod('get_due_dates');
+        
+        $this->setAdminUser();
+        $coursename = 'test course';
+        $course = $this->getDataGenerator()->create_course(array('fullname' => $coursename));
+        $eventname1 = 'assignment 1';
+        $eventdate1 = time() + 86400;
+        $assignment1 = $this->getDataGenerator()->create_module('assign',
+                array('course' => $course->id, 'name' => $eventname1, 'duedate' => $eventdate1));
+        $eventname2 = 'assignment 2';
+        $eventdate2 = time() + (2 * 86400);
+        $assignment2 = $this->getDataGenerator()->create_module('assign',
+                array('course' => $course->id, 'name' => $eventname2, 'duedate' => $eventdate2));
+        
+        $this->setUser(null);
+        
+        // Create and enrol user as student, remove capability.
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+        $role = $DB->get_record('role', array('shortname' => 'student'), 'id');
+        $this->getDataGenerator()->enrol_user($user->id, $course->id, $role->id);
+        $this->unassignUserCapability('mod/assign:view', 1, $role->id);
+        
+        $limit = 5;
+        set_config('calendar_maxevents', $limit);
+        $lookahead = 21;
+        set_config('calendar_lookahead', $lookahead);
+        
+        $actual = $getduedates->invokeArgs(null, array('token' => 'valid'));
+        
+        $this->response['response']['shouldEndSession'] = false;
+        $this->response['response']['directives'] = array(
+                array(
+                        'type' => 'Dialog.ElicitSlot',
+                        'slotToElicit' => 'else'
+                )
+        );
+        
+        $expecteda = $this->response;
+        $expecteda['response']['outputSpeech']['text'] = 'Sorry, you have no upcoming events. Would you like anything else?';
+        
+        $expectedb = $this->response;
+        $expectedb['response']['outputSpeech']['text'] = 'I apologize, but there are no upcoming events on your calendar. Do you need any other information?';
+        
+        $this->assertTrue($expecteda == $actual || $expectedb == $actual);
+    }
 }
