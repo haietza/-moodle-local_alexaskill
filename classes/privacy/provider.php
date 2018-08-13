13c5a7924b8682580d765837cf19baf27f92949f
Michelle Melton <meltonml@appstate.edu>
2018-08-12 21:45:50 -0400

    Implement privacy API

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,15 +24,15 @@
  */
 
 namespace local_alexaskill\privacy;
+
 use \core_privacy\local\metadata\collection;
-use \core_privacy\local\request\writer;
-use \core_privacy\local\request\approved_contextlist;
 
 defined('MOODLE_INTERNAL') || die();
 
 class provider implements
-    // This plugin does store personal user data.
-    \core_privacy\local\metadata\provider
+    // This plugin does store personal user data, but in the profilefield_text subsystem, which handles export and delete.
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\data_provider
 {
     /**
      * Describe user preference stored for plugin and external link user data sent to.
@@ -41,80 +41,27 @@ class provider implements
      * @return collection
      */
     public static function get_metadata(collection $collection) : collection {
-        $collection->add_user_preference('amazonalexaskillpin',
-                'privacy:metadata:preference:local_alexaskill_pin');
+        $collection->add_subsystem_link('profilefield_text', [], 'privacy:metadata:alexaskill_alexa:userinfodata');
 
         $collection->add_external_location_link('alexaskill_alexa', [
-                'accessToken' => 'privacy:metadata:alexaskill_alexa:accessToken'
+                'token' => 'privacy:metadata:alexaskill_alexa:token',
+                'firstname' => 'privacy:metadata:alexaskill_alexa:firstname',
+                'coursefullname' => 'privacy:metadata:alexaskill_alexa:coursefullname',
+                'forumdiscussionsubject' => 'privacy:metadata:alexaskill_alexa:forumdiscussionsubject',
+                'forumdiscussionmessage' => 'privacy:metadata:alexaskill_alexa:forumdiscussionmessage',
+                'gradereportoverview' => 'privacy:metadata:alexaskill_alexa:gradereportoverview',
+                'usereventsname' => 'privacy:metadata:alexaskill_alexa:usereventsname',
+                'usereventstimestart' => 'privacy:metadata:alexaskill_alexa:usereventstimestart',
+                'groupeventsname' => 'privacy:metadata:alexaskill_alexa:groupeventsname',
+                'groupeventstimestart' => 'privacy:metadata:alexaskill_alexa:groupeventstimestart',
+                'courseeventsname' => 'privacy:metadata:alexaskill_alexa:courseeventsname',
+                'courseeventstimestart' => 'privacy:metadata:alexaskill_alexa:courseeventstimestart',
+                'categoryeventsname' => 'privacy:metadata:alexaskill_alexa:categoryeventsname',
+                'categoryeventstimestart' => 'privacy:metadata:alexaskill_alexa:categoryeventstimestart',
+                'siteeventsname' => 'privacy:metadata:alexaskill_alexa:siteeventsname',
+                'siteeventstimestart' => 'privacy:metadata:alexaskill_alexa:siteeventstimestart'
         ], 'privacy:metadata:alexaskill_alexa');
 
         return $collection;
-    }
-
-    /**
-     * Get the list of contexts that contain user information for the specified user.
-     *
-     * @param int $userid The user to search.
-     * @return contextlist $contextlist The list of contexts used in this plugin.
-     */
-    public static function get_contexts_for_userid(int $userid) : \core_privacy\local\request\contextlist {
-        $contextlist = new \core_privacy\local\request\contextlist();
-
-        $contextlist->add_user_context($userid);
-        return $contextlist;
-    }
-
-    /**
-     * Export all user preferences for the plugin.
-     *
-     * @param int $userid The userid of the user whose data is to be exported.
-     */
-    public static function export_user_preferences(int $userid) {
-        $alexapin = get_user_preferences('amazonalexaskillpin', null, $userid);
-        if (null !== $alexapin) {
-            $alexapindescription = get_string('alexaskill_accountlinking_pin_help', 'local_alexaskill');
-        }
-        writer::export_user_preference('local_alexaskill', 'amazonalexaskillpin', $alexapin, $alexapindescription);
-    }
-
-    /**
-     * Delete all personal data for all users in the specified context.
-     *
-     * @param context $context Context to delete data from.
-     */
-    public static function delete_data_for_all_users_in_context(\context $context) {
-        global $DB;
-
-        if ($context->contextlevel != CONTEXT_SYSTEM) {
-            return;
-        }
-
-        $field = $DB->get_record('user_info_field', array('shortname' => 'amazonalexaskillpin'), 'id');
-        if (!$field) {
-            return;
-        }
-
-        $DB->delete_records('user_info_data', array('fieldid' => $field->id));
-    }
-    
-    /**
-     * Delete all personal data for user in specified context.
-     * 
-     * @param approved_contextlist $contextlist
-     */
-    public static function delete_data_for_user(approved_contextlist $contextlist) {
-        global $DB;
-
-        if (empty($contextlist->count())) {
-            return;
-        }
-
-        $field = $DB->get_record('user_info_field', array('shortname' => 'amazonalexaskillpin'), 'id');
-        if (!$field) {
-            return;
-        }
-
-        $userid = $contextlist->get_user()->id;
-        $DB->delete_records('user_info_data', array('userid' => $userid, 'fieldid' => $field->id));
     }
 }
